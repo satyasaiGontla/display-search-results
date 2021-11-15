3cbc6982f83d6f450ec1cb17413ce70a930e444e
rakesh <gontlarakesh@gmail.com>
2021-11-15 08:29:40 +0000

    search and delete user list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import UserProfile from './components/UserProfile'
 
 import './App.css'
 
-const userDetailsList = [
+const initialUserDetailsList = [
   {
     uniqueNo: 1,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
@@ -33,6 +33,7 @@ const userDetailsList = [
 class App extends Component {
   state = {
     searchInput: '',
+    usersDetailsList: initialUserDetailsList,
   }
 
   onChangeSearchInput = event => {
@@ -41,10 +42,19 @@ class App extends Component {
     })
   }
 
+  deleteUser = uniqueNo => {
+    const {usersDetailsList} = this.state
+    const filteredUsersData = usersDetailsList.filter(
+      each => each.uniqueNo !== uniqueNo,
+    )
+    this.setState({
+      usersDetailsList: filteredUsersData,
+    })
+  }
+
   render() {
-    const {searchInput} = this.state
-    console.log(searchInput)
-    const searchResults = userDetailsList.filter(eachUser =>
+    const {searchInput, usersDetailsList} = this.state
+    const searchResults = usersDetailsList.filter(eachUser =>
       eachUser.name.includes(searchInput),
     )
     return (
@@ -57,7 +67,11 @@ class App extends Component {
         />
         <ul className="list-container">
           {searchResults.map(eachUser => (
-            <UserProfile userDetails={eachUser} key={eachUser.uniqueNo} />
+            <UserProfile
+              userDetails={eachUser}
+              key={eachUser.uniqueNo}
+              deleteUser={this.deleteUser}
+            />
           ))}
         </ul>
       </div>
--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,8 +1,11 @@
 import './index.css'
 
 const UserProfile = props => {
-  const {userDetails} = props
-  const {imageUrl, name, role} = userDetails
+  const {userDetails, deleteUser} = props
+  const {imageUrl, name, role, uniqueNo} = userDetails
+  const onDelete = () => {
+    deleteUser(uniqueNo)
+  }
   return (
     <li className="user-card-container">
       <img src={imageUrl} className="profile-pic" alt="profile-pic" />
@@ -10,7 +13,15 @@ const UserProfile = props => {
         <h1 className="user-name"> {name} </h1>
         <p className="user-designation"> {role} </p>
       </div>
+      <button className="delete-button" onClick={onDelete}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/cross-img.png"
+          alt="cross"
+          className="delete-img"
+        />
+      </button>
     </li>
   )
 }
+
 export default UserProfile
